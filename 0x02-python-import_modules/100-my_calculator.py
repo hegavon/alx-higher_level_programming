3c33b4630f02f9562346e524671c6cc716f36075
hegavon <victorgavon@gmail.com>
2023-11-30 18:23:43 -0800

    0x02. Python - import & modules Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,29 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-def main():
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
+if (__name__ == "__main__"):
+    argc = len(argv)
+    result = 0
+    operators = ["+", "-", "*", "/"]
 
-    if len(argv) != 4:
+    if (argc != 4):
         print(f"Usage: {argv[0]} <a> <operator> <b>")
         exit(1)
 
-    num1, operator, num2 = map(int, argv[1:4])
-
-    if operator not in operators:
+    if (argv[2] not in operators):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    result = operators[operator](num1, num2)
-    print(f"{num1} {operator} {num2} = {result}")
+    num1 = int(argv[1])
+    operator = argv[2]
+    num2 = int(argv[3])
 
-if __name__ == "__main__":
-    main()
+    if (operator == "+"):
+        result = add(num1, num2)
+    elif (operator == "-"):
+        result = sub(num1, num2)
+    elif (operator == "*"):
+        result = mul(num1, num2)
+    else:
+        result = div(num1, num2)
+    print("{:d} {:s} {:d} = {:d}".format(num1, operator, num2, result))
