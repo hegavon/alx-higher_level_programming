1a1b6ff283007c3e37a7ad30854851df97297f32
hegavon <victorgavon@gmail.com>
2023-12-04 13:57:46 -0800

    0x03. Python - Data Structures: Lists, Tuples Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return (None)
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
 
-    length = len(my_list)
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+index = 2
+result = element_at(my_list, index)
 
-    if idx > length - 1:
-        return (None)
-
-    return(my_list[idx])
+print(result)
