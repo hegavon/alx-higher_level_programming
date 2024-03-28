34006cc551f0c598015ba162d7a515df6c9f5578
Victor Amajuoyi <victorgavon@gmail.com>
2024-03-28 08:47:08 -0700

    0x11-python-network_1 - Task 4. update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+"""
+Python script that fetches URL with requests package
+"""
 import requests
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    content = response.text
 
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
