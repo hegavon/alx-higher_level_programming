98d2b4f353b6f7a45b706150bbaf42e7ca8d68d0
Victor Amajuoyi <victorgavon@gmail.com>
2024-03-28 09:50:21 -0700

    0x11-python-network_1 - Task 10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+
 """
-This script retrieves the user ID from the GitHub
-API using the provided username and password.
-Usage: ./10-my_github.py [github_username] [github_password]
+This script uses the GitHub API to list the last 10 commits
+for a given repository and owner.
+Usage: ./100-github_commits.py [github_repo] [github_owner]
 """
+
 from sys import argv
 import requests
-from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/user'
-    r = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    r = requests.get(url)
+    list_commits = r.json()
+    for commit in list_commits[0:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
