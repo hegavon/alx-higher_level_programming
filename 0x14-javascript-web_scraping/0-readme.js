cf88142e8431f78a3f6d3b8974017873dd21388f
Victor Amajuoyi <victorgavon@gmail.com>
2024-04-23 21:43:33 +0100

    0x14-javascript-web_scraping Task 0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,11 @@
 #!/usr/bin/node
-// Read and print file content
 
 const fs = require('fs');
 
-// Check if file path argument is provided
-if (process.argv.length < 3) {
-  console.log("Usage: ./script.js <file_path>");
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-
-// Read file content
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error("An error occurred while reading the file:", err);
-  } else {
-    console.log(data);
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
